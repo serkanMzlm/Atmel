06e2d54c5b465946cc327c3e2505b9162419643e
serkanMzlm <74418302+serkanMzlm@users.noreply.github.com>
2021-01-10 17:36:20 +0300

    Add files via upload

---
+++ ATtiny85/Attiny85.ino
@@ -0,0 +1,30 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+ 
+}
+
+
+void loop() {
+ /* 
+  DigiKeyboard.sendKeyStroke(0); // Bir tane boş tuşa basar 
+  DigiKeyboard.sendKeyStroke(0,MOD_GUI_LEFT); // Windows tuşu  
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT);//Sol kontrol
+  DigiKeyboard.sendKeyStroke(MOD_SHIFT_LEFT);//Sol shift
+  DigiKeyboard.sendKeyStroke(MOD_ALT_LEFT);//Sol alt
+  DigiKeyboard.sendKeyStroke(MOD_GUI_LEFT);//Windows tuşu
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_RIGHT); 
+  DigiKeyboard.sendKeyStroke(MOD_SHIFT_RIGHT);
+  DigiKeyboard.sendKeyStroke(MOD_ALT_RIGHT);
+  DigiKeyboard.sendKeyStroke(MOD_GUI_RIGHT);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);//Enter
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);//Space
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);//Yön tuşları
+  DigiKeyboard.sendKeyStroke(KEY_A);//a tuşu
+  DigiKeyboard.sendKeyStroke(KEY_2);//2 tuşu 
+  DigiKeyboard.sendKeyStroke(KEY_F1);//F tuşları 
+  DigiKeyboard.delay(500); //bekleme
+*/
+DigiKeyboard.sendKeyStroke(KEY_ARROW_TOP);
+while (true);
+}
